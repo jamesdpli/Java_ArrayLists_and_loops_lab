c96d3ab0729ff2e5f3d8678258b4318373d84c16
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-04-19 16:42:40 +0100

    initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,122 @@
+//upload to GitHub
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+//        SCOTTISH ISLANDS
+        List<String> scottishIslands = new ArrayList<>();
+        scottishIslands.add("Jura");
+        scottishIslands.add("Mull");
+        scottishIslands.add("Skye");
+        scottishIslands.add("Arran");
+        scottishIslands.add("Tresco");
+
+//        1. Add "Coll" to the end of the list
+        scottishIslands.add("Coll");
+//        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0,"Tiree");
+//        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add(2,"Islay");
+//        4. Print out the index position of "Skye"
+        System.out.println(scottishIslands.indexOf("Skye"));
+//        5. Remove "Tresco" from the list by name
+        scottishIslands.remove("Tresco");
+//        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
+//        7. Print the number of islands in your arraylist
+        System.out.println(scottishIslands.size());
+//        8. Sort the list alphabetically
+        //Collections.sort(scottishIslands); -> shorter way of writing it out
+        java.util.Collections.sort(scottishIslands, Collator.getInstance());
+
+//        9. Print out all the islands using a for loop
+        for (String scottishIsland : scottishIslands ) {
+            System.out.println(scottishIsland);
+        }
+        System.out.println(scottishIslands);
+
+//        NUMBERS
+        List<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
+
+        System.out.println("numbers: " + numbers);
+
+//        1. Print out a list of the even integers
+        // if you want it to print on the same line -> List<Integer> evenNumbers = new ArrayList<>();
+        for (int i = 0; i< numbers.size(); i++){
+
+            if (numbers.get(i) % 2 == 0){
+                System.out.println("Even number: " + numbers.get(i));
+            }
+        }
+//        2. Print the difference between the largest and smallest value
+        int diffMaxMin = Collections.max(numbers) - Collections.min(numbers);
+        System.out.println("The difference of the max and min is: " + diffMaxMin);
+
+//        3. Print true if the list contains a 1 next to a 1 somewhere
+        boolean isNextToOne = true;
+        for (int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i) == 1 ){
+                if (numbers.get(++i) == 1){
+                    System.out.println(isNextToOne);
+                }
+            }
+        }
+//        4. Print the sum of the numbers
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++){
+            sum += numbers.get(i);
+        }
+        System.out.println(sum);
+//        5. Print the sum of the numbers...
+//           ...except the number 13 is unlucky, so it does not count...
+//           ...and numbers that come immediately after a 13 also do not count
+//          HINT - You will need to track the index throughout the loop
+//
+//          So [7, 13, 2] would have sum of 9.
+
+        // check whether a number is 13
+        // if tracking the index, that if I hit 13, stop incrementing the index
+
+        int total = 0;
+        int index = 0;
+        for (int number : numbers) {
+            if (number == 13 || numbers.get(index) == 13) {
+                continue;
+            } else {
+                total += number;
+                index += 1;
+            }
+        }
+        System.out.println(total);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
